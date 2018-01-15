67702c26b7b90b32b996d90a9e48363db43cdaaf
Pradish Bijukchhe <pradishbijukchhe@gmail.com>
2018-01-15 22:48:40 +0545

    Adds key listener and images

--- hangman.py
+++ hangman.py
@@ -17,14 +17,25 @@ pygame.display.set_caption('Python Hangman')
 
 font = pygame.font.Font(None, 36)
 title = font.render("The Hangman Game", 1, (255, 255, 255))
+
+#game init vars
 answer = "guitarhero"
 current = len(answer)*"_ "
+found = len(answer)*[False]
+incorrect = 0
 
 while 1:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+    event = pygame.event.wait()
+    if event.type == pygame.QUIT: sys.exit()
+    if event.type == pygame.KEYDOWN:
+        pressed = pygame.key.get_pressed()
+        for index,x in enumerate(pressed[pygame.K_a:pygame.K_z+1]):
+            if x == True:
+                input_key = pygame.key.name(pygame.K_a+index)
+    hangman_img = pygame.image.load("data/Hangman-"+str(incorrect)+".png")
     current_text = font.render(current, 1, (255, 255, 255))
     screen.fill((0,0,0))
+    screen.blit(hangman_img,(50,200))
     screen.blit(title, title.get_rect(centerx=screen.get_width()/2,y=50))
-    screen.blit(current_text, current_text.get_rect(centerx=screen.get_width()/2,centery=screen.get_height()/2))
+    screen.blit(current_text, current_text.get_rect(centerx=width/2,centery=height/2))
     pygame.display.flip()
